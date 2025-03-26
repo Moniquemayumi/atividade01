405da0b9aa78c33807b36029d669ce191c290c46
Moniquemayumi <163213589+Moniquemayumi@users.noreply.github.com>
2025-03-26 15:30:31 -0300

    Add files via upload

---
+++ exercicios1/atv1.py
@@ -0,0 +1,8 @@
+nota1 = float(input("Digite sua nota 1: "))
+nota2 = float(input("Digite sua nota 2: "))
+nota3 = float(input("Digite sua nota 3: "))
+media_arit= nota1+nota2+nota3/3
+media_pond1=nota1*2+nota2*2+nota3*3/3
+media_pond2=nota1*1+nota2*2+nota3*2/3
+print(f"A média aritmétrica é: {media_arit}; A média ponderada 1 é: {media_pond1} e a A média ponderada 2 é: {media_pond2} ")
+
